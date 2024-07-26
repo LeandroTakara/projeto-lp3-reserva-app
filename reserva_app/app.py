394c79981262566abd1b94530f90ee319d53bd5b
Leandro Kenzo Takara <leandroktkr@gmail.com>
2024-07-25 21:13:44 -0300

    Create user sign in and login

--- reserva_app/app.py
+++ reserva_app/app.py
@@ -1,28 +1,59 @@
 from flask import Flask, render_template, request
+from save_load import save_csv, load_csv
+from typing import Callable
 
-app = Flask(__name__, template_folder='templates')
+def contains_register(registers: list[str], predicate: Callable[[], bool]) -> bool:
+    for register in registers:
+        if predicate(register):
+            return True
+    
+    return False
 
-@app.route('/')
+
+LOCAL_DATABASE_USERS_PATH = 'csv-database/users.csv'
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
 def login_page():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
 
-@app.route('/', methods=['POST'])
-def login_page_by_post():
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-@app.route('/cadastro')
+        users = load_csv(LOCAL_DATABASE_USERS_PATH)
+
+        correct_login = contains_register(users, lambda user: user[1] == email and user[2] == password)
+        
+        if correct_login:
+            return render_template('reservas.html')
+
+        return render_template('login.html')
+
+    raise Exception(f'Invalid {request.method} method in login_page()')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro_page():
-    return render_template('cadastro.html')
+    if request.method == 'GET':
+        return render_template('cadastro.html')
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = [nome, email, password]
+
+        save_csv(LOCAL_DATABASE_USERS_PATH, user)
+
+        return render_template('login.html')
+
+    raise Exception(f'Invalid {request.method} method in cadastro_page()')
 
 @app.route('/reservas')
 def reservas_page():
-    return render_template('reservas.html')
-
-@app.route('/reservas', methods=['POST'])
-def reservas_page_by_post():
-    email = request.form['email']
-    password = request.form['password']
-
     return render_template('reservas.html')
 
 @app.route('/reservar-sala')
@@ -40,3 +71,6 @@ def cadastrar_sala_page():
 @app.route('/reserva/detalhe-reserva')
 def reserva_detalhe_reserva_page():
     return render_template('reserva/detalhe-reserva.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
